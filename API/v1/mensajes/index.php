f17b02761cce336908094eaff746bbc17f641684
Dadow <dadow@koero.org>
2019-06-05 14:49:47 +0200

    [add] api/index

--- API/v1/mensajes/index.php
+++ API/v1/mensajes/index.php
@@ -1,6 +1,27 @@
 <?php 
-$values = json_decode(file_get_contents('php://input'),True);
-//datos
+$data = [];
+if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+    $hostPDO = "sqlite:../../../littlechat22test.sqlite";
+    $miPDO = new PDO($hostPDO);
+    header('Content-Type: application/json');
 
-header('Content-Type: application/json');
-echo json_encode($data);
+    if(isset($_REQUEST['id'])){
+        $id = (int)$_REQUEST['id'];
+            if($id>0){
+                $miConsulta = $miPDO->prepare('SELECT * FROM mensajes WHERE id = :id;');
+                $miConsulta->execute(array('id'=>$_REQUEST["id"]));
+                $data = $miConsulta->fetch(PDO::FETCH_ASSOC);
+                echo json_encode(array("code"=>"200","data"=>$data),JSON_PRETTY_PRINT);
+            }else{
+                http_response_code(404);
+                echo json_encode(array("code"=>"404","err_msg"=>"No hay mensajes",JSON_PRETTY_PRINT));
+            }
+        
+
+    }else{
+        $miConsulta = $miPDO->prepare('SELECT * FROM mensajes;');
+        $miConsulta->execute();
+        $data = $miConsulta->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode(array("code"=>"200","data"=>$data),JSON_PRETTY_PRINT);
+    }
+}
