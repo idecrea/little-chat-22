57812cbffc856e2569e59c92a0c0fa014c4b1bac
root <root@DESKTOP-I1RLS17.localdomain>
2019-06-13 15:07:42 +0200

    img+circulo

---
+++ index-respuesta.php
@@ -0,0 +1,51 @@
+<?php
+    require_once('head.php');
+    require_once('menu.php');
+?>
+
+        <section class="h-screen bg-teal-100 m-0 overflow-scroll">
+            <div class="flex items-center flex-col fuente-medium mb-6">
+                <div class="bg-teal-200 w-11/12 mt-4 px-2 text-teal-800">
+                    <div class="flex flex-row items-center mt-2">
+                        <div class="hidden circulo1 flex justify-center items-center"><img src="img/2.jpg" alt="" class="hidden"></div>
+                        <div class="flex justify-center items-center"><i class="fas fa-user-circle fa-4x"></i></div>
+                        <h2 class="ml-2">Username</h2>
+                    </div>
+                    <div class="fuente mb-4">
+                        <hr class="h-1 border border-teal-800 bg-teal-800">
+                        <h3 class="text-right mr-2 mb-1">21/04/1954</h3>
+                    </div>
+                    <div class="mb-2">
+                        <p class="fuente leading-snug">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perspiciatis quam minus dolore, veritatis illum.</p>
+                    </div>
+                </div>
+                <div class="flex justify-center px-3 mt-4">
+                    <button><i class="fas fa-hand-spock fa-2x text-teal-400 ml-1"></i></button>
+                    <p class="text-teal-800 text-center">0 me gusta</p>
+                    <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-2 ml-1 text-xs">Escribir comentario</button>
+                    <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-1 ml-2 text-xs px-2">Ver comentarios</button>
+                </div>
+            </div>
+            <div class="flex items-center flex-col fuente-medium mt-4 mb-8">
+                <div class=" w-11/12 mt-4 px-2 text-teal-800">
+                    <div class="flex flex-row items-center mt-2">
+                        <div class="circulo2 flex justify-center items-center"><img src="img/39.jpg" alt=""></div>
+                        <h2 class="ml-2">Username</h2>
+                    </div>
+                    <div class="fuente mb-4">
+                        <hr class="h-0 border border-teal-800 bg-teal-800">
+                        <h3 class="text-right mr-2 mb-1">21/04/1954</h3>
+                    </div>
+                    <div class="mb-2">
+                        <p class="fuente leading-snug">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perspiciatis quam minus dolore, veritatis illum.</p>
+                    </div>
+                </div>
+                <div class="flex justify-start items-center px-3 mt-2 w-screen ml-1">
+                    <button><i class="fas fa-hand-spock fa-2x text-teal-400 ml-1"></i></button>
+                    <p class="text-teal-800 text-center ml-6">0 me gusta</p>
+                </div>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,24 +4,26 @@
 ?>
 
         <section class="h-screen bg-teal-100 m-0 flex items-center flex-col fuente-medium overflow-scroll">
-            <div class="bg-teal-200 w-11/12 mt-4 px-2 text-teal-800">
-                <div class="flex flex-row items-center mt-2">
-                    <img class="w-12 h-12" src="img/circulo.png" alt="">
-                    <h2 class="ml-2">Username</h2>
+            <div class="flex items-center flex-col fuente-medium mb-6">
+                <div class="bg-teal-200 w-11/12 mt-4 px-2 text-teal-800">
+                    <div class="flex flex-row items-center mt-2">
+                        <div class="circulo1 flex justify-center items-center"><img src="img/1.jpg" alt=""></div>
+                        <h2 class="ml-2">Username</h2>
+                    </div>
+                    <div class="fuente mb-4">
+                        <hr class="h-1 border border-teal-800 bg-teal-800">
+                        <h3 class="text-right mr-2 mb-1">21/04/1954</h3>
+                    </div>
+                    <div class="mb-2">
+                        <p class="fuente leading-snug">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perspiciatis quam minus dolore, veritatis illum.</p>
+                    </div>
                 </div>
-                <div class="fuente mb-4">
-                    <hr class="h-1 border border-teal-800 bg-teal-800">
-                    <h3 class="text-right mr-2 mb-1">21/04/1954</h3>
+                <div class="flex justify-center px-3 mt-4">
+                    <button><i class="fas fa-hand-spock fa-2x text-teal-400 ml-1"></i></button>
+                    <p class="text-teal-800 text-center">0 me gusta</p>
+                    <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-2 ml-1 text-xs">Escribir comentario</button>
+                    <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-1 ml-2 text-xs px-2">Ver comentarios</button>
                 </div>
-                <div class="mb-2">
-                    <p class="fuente leading-snug">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perspiciatis quam minus dolore, veritatis illum.</p>
-                </div>
-            </div>
-            <div class="flex justify-center px-2 mt-2">
-                <i class="fas fa-hand-spock fa-2x text-teal-400 ml-1"></i>
-                <p class="text-teal-800 text-center">0 me gusta</p>
-                <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-2 ml-1 text-xs">Escribir comentario</button>
-                <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-1 ml-2 text-xs px-2">Ver comentarios</button>
             </div>
         </section>
     </div>
