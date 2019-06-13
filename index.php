ab2bd2e6cc5a4d228d2dac77cfd979b25565d729
root <root@DESKTOP-I1RLS17.localdomain>
2019-06-13 13:54:44 +0200

    mensaje.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    require_once('head.php');
+    require_once('menu.php');
+?>
+
+        <section class="h-screen bg-teal-100 m-0 flex items-center flex-col fuente-medium">
+            <div class="bg-teal-200 w-11/12 mt-4 px-2 text-teal-800">
+                <div class="flex flex-row items-center mt-2">
+                    <img class="w-12 h-12" src="img/circulo.png" alt="">
+                    <h2 class="ml-2">Username</h2>
+                </div>
+                <div class="fuente mb-4">
+                    <hr class="h-1 border border-teal-800 bg-teal-800">
+                    <h3 class="text-right mr-2 mb-1">21/04/1954</h3>
+                </div>
+                <div class="mb-2">
+                    <p class="fuente leading-snug">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero perspiciatis quam minus dolore, veritatis illum.</p>
+                </div>
+            </div>
+            <div class="flex justify-center px-2 mt-2">
+                <i class="fas fa-hand-spock fa-2x text-teal-400 ml-1"></i>
+                <p class="text-teal-800 text-center">0 me gusta</p>
+                <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-2 ml-1 text-xs">Escribir comentario</button>
+                <button class="fuente-bold text-teal-100 border border-teal-400 bg-teal-400 mr-1 ml-2 text-xs px-2">Ver comentarios</button>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
